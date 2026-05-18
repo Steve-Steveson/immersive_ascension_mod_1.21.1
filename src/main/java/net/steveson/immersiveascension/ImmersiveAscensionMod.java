a0bf4c0ae5284f8d5830223a8058f50016b26321
Steve-Steveson <mr.steve.steveson@gmail.com>
2026-05-17 21:26:00 -0400

    default config deleted items moved to IE's creative tab

--- src/main/java/net/steveson/immersiveascension/ImmersiveAscensionMod.java
+++ src/main/java/net/steveson/immersiveascension/ImmersiveAscensionMod.java
@@ -1,8 +1,11 @@
 package net.steveson.immersiveascension;
 
 import blusunrize.immersiveengineering.api.EnumMetals;
+import blusunrize.immersiveengineering.common.register.IECreativeTabs;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.DyeColor;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.steveson.immersiveascension.block.ModBlocks;
 import net.steveson.immersiveascension.item.ModItems;
 import org.slf4j.Logger;
@@ -26,43 +29,12 @@ public class ImmersiveAscensionMod {
     public static final String MOD_ID = "immersive_ascension";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
-//    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
-//    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
-//    // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
-//    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
-//    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "examplemod" namespace
-//    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
-//
-//    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
-//    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
-//    // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
-//    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
-//
-//    // Creates a new food item with the id "examplemod:example_id", nutrition 1 and saturation 2
-//    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder()
-//            .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
-//
-//    // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
-//    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-//            .title(Component.translatable("itemGroup.examplemod")) //The language key for the title of your CreativeModeTab
-//            .withTabsBefore(CreativeModeTabs.COMBAT)
-//            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-//            .displayItems((parameters, output) -> {
-//                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-//            }).build());
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public ImmersiveAscensionMod(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-
-//        // Register the Deferred Register to the mod event bus so blocks get registered
-//        BLOCKS.register(modEventBus);
-//        // Register the Deferred Register to the mod event bus so items get registered
-//        ITEMS.register(modEventBus);
-//        // Register the Deferred Register to the mod event bus so tabs get registered
-//        CREATIVE_MODE_TABS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
@@ -74,59 +46,46 @@ public class ImmersiveAscensionMod {
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(FMLCommonSetupEvent event) {
-//        // Some common setup code
-//        LOGGER.info("HELLO FROM COMMON SETUP");
-//
-//        if (Config.LOG_DIRT_BLOCK.getAsBoolean()) {
-//            LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
-//        }
-//
-//        LOGGER.info("{}{}", Config.MAGIC_NUMBER_INTRODUCTION.get(), Config.MAGIC_NUMBER.getAsInt());
-//
-//        Config.ITEM_STRINGS.get().forEach((item) -> LOGGER.info("ITEM >> {}", item));
+
     }
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-            event.accept(ModBlocks.INSULATING_GLASS_STAIRS);
-            event.accept(ModBlocks.SLAG_GLASS_STAIRS);
-            event.accept(ModBlocks.SLAG_GLASS_SLAB);
-            for(EnumMetals m : EnumMetals.values())
-            {
-                if(!m.isVanillaMetal())
-                {
-                    event.accept(ModBlocks.STORAGE_STAIRS.get(m));
+        for (DeferredHolder<CreativeModeTab, ? extends CreativeModeTab> tab : IECreativeTabs.REGISTER.getEntries()){
+            if (event.getTabKey() == tab.getKey()) {
+                event.accept(ModBlocks.INSULATING_GLASS_STAIRS);
+                event.accept(ModBlocks.SLAG_GLASS_STAIRS);
+                event.accept(ModBlocks.SLAG_GLASS_SLAB);
+                for (EnumMetals m : EnumMetals.values()) {
+                    if (!m.isVanillaMetal()) {
+                        event.accept(ModBlocks.STORAGE_STAIRS.get(m));
+                    }
+                    event.accept(ModBlocks.SHEETMETAL_STAIRS.get(m));
                 }
-                event.accept(ModBlocks.SHEETMETAL_STAIRS.get(m));
+                for (DyeColor dye : DyeColor.values()) {
+                    event.accept(ModBlocks.COLORED_SHEETMETAL_STAIRS.get(dye));
+                }
+                event.accept(ModBlocks.COKE_STAIRS);
+                event.accept(ModBlocks.COKEBRICK_STAIRS);
+                event.accept(ModBlocks.BLASTBRICK_STAIRS);
+                event.accept(ModBlocks.BLASTBRICK_REINFORCED_STAIRS);
+                event.accept(ModBlocks.ALLOYBRICK_STAIRS);
+                event.accept(ModBlocks.CUT_URANIUM_BLOCK);
+                event.accept(ModBlocks.CUT_URANIUM_STAIRS);
+                event.accept(ModBlocks.CUT_URANIUM_SLAB);
+                event.accept(ModBlocks.PILLAR_URANIUM_BLOCK);
+                event.accept(ModBlocks.PILLAR_URANIUM_STAIRS);
+                event.accept(ModBlocks.PILLAR_URANIUM_SLAB);
             }
-            for(DyeColor dye : DyeColor.values()){
-                event.accept(ModBlocks.COLORED_SHEETMETAL_STAIRS.get(dye));
-            }
-            event.accept(ModBlocks.COKE_STAIRS);
-            event.accept(ModBlocks.COKEBRICK_STAIRS);
-            event.accept(ModBlocks.BLASTBRICK_STAIRS);
-            event.accept(ModBlocks.BLASTBRICK_REINFORCED_STAIRS);
-            event.accept(ModBlocks.ALLOYBRICK_STAIRS);
-            event.accept(ModBlocks.CUT_URANIUM_BLOCK);
-            event.accept(ModBlocks.CUT_URANIUM_STAIRS);
-            event.accept(ModBlocks.CUT_URANIUM_SLAB);
-            event.accept(ModBlocks.PILLAR_URANIUM_BLOCK);
-            event.accept(ModBlocks.PILLAR_URANIUM_STAIRS);
-            event.accept(ModBlocks.PILLAR_URANIUM_SLAB);
         }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
-//        // Do something when the server starts
-//        LOGGER.info("HELLO from server starting");
+
     }
 }
